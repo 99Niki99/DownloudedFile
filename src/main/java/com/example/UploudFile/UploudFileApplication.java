fd1bdd4eaab7f06458e11b3eb08f8e30e1227368
Nikita <ltkmabyf1979@GMAIL.COM>
2023-05-14 22:31:25 -0500

    new project

---
+++ src/main/java/com/example/UploudFile/UploudFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.UploudFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploudFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploudFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UploudFile/UploudFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UploudFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploudFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
